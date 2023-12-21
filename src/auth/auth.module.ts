0ea0d03af75ae097ea2b65338456367bcfd900ff
LOVESH1808 <loveshdutta1808@gmail.com>
2023-12-21 23:22:05 +0530

    first commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,14 @@
+import { Global, Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { PrismaModule } from "src/prisma/prisma.module";
+import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "./strategy";
+
+@Global()
+@Module({
+    imports: [JwtModule.register({})],
+    controllers: [AuthController],
+    providers: [AuthService,JwtStrategy],
+})
+export class AuthModule {};
